0fad5f34c0457bd14ae86960e6c1be19155d4d2b
trongdev <hiiamtrong@gmail.com>
2025-05-12 22:32:06 +0700

    feat(achievement): add IDs to achievements in seed data and update admin seeder to prevent duplicate entries

--- src/database/seeds/admin.seed.ts
+++ src/database/seeds/admin.seed.ts
@@ -5,8 +5,12 @@ import { Seeder } from "typeorm-extension";
 export default class AdminSeeder implements Seeder {
     public async run(dataSource: DataSource): Promise<any> {
         const repository = dataSource.getRepository(AdminEntity);
-   
+
         // hash password: spidex@tothemoon
+        const existingAdmin = await repository.findOne({ where: { username: 'admin' } });
+        if (existingAdmin) {
+            return;
+        }
         await repository.insert({
             username: 'admin',
             password: '$2b$10$NPH.aMaH3HEhy29Om2acBOjHdWpfVxMa2GzX75Zl0xlqXReqo7NI2',
