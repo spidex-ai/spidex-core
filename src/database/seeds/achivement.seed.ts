40fadd5dd943b1db38da09ede122378473cd5a94
trongdev <hiiamtrong@gmail.com>
2025-05-12 22:30:12 +0700

    feat(achievement): update achievement names, icons, and descriptions; modify seeder to include base URL for icons

--- src/database/seeds/achivement.seed.ts
+++ src/database/seeds/achivement.seed.ts
@@ -4,7 +4,10 @@ import { Seeder } from 'typeorm-extension';
 import achievementData from '../data-seed/achievement.seed.json';
 export default class AchievementSeeder implements Seeder {
     public async run(dataSource: DataSource): Promise<any> {
+        for (const achievement of achievementData) {
+            achievement.icon = `${process.env.APP_BASE_URL}${achievement.icon}`;
+        }
         const repository = dataSource.getRepository(AchievementEntity);
-        await repository.insert(achievementData);
+        await repository.save(achievementData);
     }
 }
